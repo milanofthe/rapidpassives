952427fd7e742eff377caef5c92f7720ba60f054
Milan Rother <milan.rother@gmx.de>
2026-03-25 15:33:59 +0100

    Remove wasm plugin from main plugins (only needed in worker)

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,15 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import wasm from 'vite-plugin-wasm';
 import { defineConfig } from 'vite';
-import path from 'path';
 
 export default defineConfig({
-	plugins: [wasm(), sveltekit()],
-	resolve: {
-		alias: {
-			'gds-wasm': path.resolve(__dirname, '../wasm/pkg'),
-		},
-	},
+	plugins: [sveltekit()],
 	worker: {
 		plugins: () => [wasm()],
 	},
